f016e310823efbf613fe7d99d03d2616f2f1d047
TulaniSouza <87509369+TulaniSouza@users.noreply.github.com>
2022-07-12 15:59:02 -0300

    Add files via upload

---
+++ english VNW/script.js
@@ -0,0 +1,17 @@
+let comedy =[{
+    title: 'As Branquelas',
+    diretor: 'Keenen Ivory Wayans'
+    year:2004
+}]
+
+let comedyRomantic=[{
+    title: 'O Casamento do meu melhor amigo'
+    diretor: 'P.J. Hogan'
+    year:1997
+}]
+
+let action=[{
+    title:'velozes e furiosos'
+    diretor: 'Justin Lin'
+    year:2001
+}]
